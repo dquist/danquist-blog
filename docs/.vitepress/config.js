36a9570a73dc3a0e975e6e740d259b8f79a08598
Dan Quist <dan.quist@proton.me>
2022-11-27 21:31:03 -0600

    Convert to VitePress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+export default {
+    lang: 'en-US',
+    title: "Dan's Personal Blog",
+    themeConfig: {
+      siteTitle: "Home",
+      nav: [
+        {
+          text: 'Articles',
+          items: [
+            { text: 'Getting Started', link: '/getting-started' },
+          ]
+        },
+        { text: 'About', link: '/about' },
+      ],
+      socialLinks: [
+        { icon: 'twitter', link: 'https://twitter.com/TailLatency' },
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/danquistmn/' },
+      ]
+    }
+  }
---
+++ docs/about.md
@@ -0,0 +1,5 @@
+# About Dan
+
+Dan is currently a senior software engineer at Amazon Freight where he works on profitably externalizing Amazon's middle-mile transportation network for external shippers.
+
+<img src="./img/profile-sf.jpg" width="500">
